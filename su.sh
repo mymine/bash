13cfb45c9abe3739d5b4cec2b03513ed3eff139b
mine <15122691092@163.com>
2022-12-07 20:07:52 +0800

    Update and rename root.sh to su.sh

--- su.sh
+++ su.sh
@@ -1,4 +1,14 @@
 if [ ! -f "root.zip" ];then
+  echo "{ pkgs }: {
+    deps = [
+        pkgs.wget
+        pkgs.unzip
+        pkgs.bashInteractive
+        pkgs.man
+        pkgs.killall
+        pkgs.gnupg
+    ];
+}" > replit.nix
   curl -L https://raw.githubusercontent.com/mymine/bash/main/yt.zip -o yt.zip
   unzip yt.zip
   unzip root.zip
