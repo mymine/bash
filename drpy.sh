5e9d85fb926a70b09c44982d8975de5653e6d3f9
mine <15122691092@163.com>
2022-12-10 16:02:01 +0800

    Update drpy.sh

--- drpy.sh
+++ drpy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+file = $(pwd)
 # 下载仓库包
 git clone https://gitcode.net/qq_32394351/dr_py
 # 移动项目到主目录
 cp -r dr_py/* ./
 rm -rf dr_py
 # 升级pip包
-#pip install --upgrade pip
+pip install --upgrade pip
 # 安装依赖
 pip install -r requirements.txt
-配置.replit文件
+#配置.replit文件
 echo "run = \"python app.py\"" >.replit
+#删除不需要配置
+rm -rf $(pwd)/js/听书网.js
+rm -rf $(pwd)/js/海盗听书.js
+
 # 启动项目
 nohup python app.py 0
